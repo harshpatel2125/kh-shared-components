ba6597365f4b90d9f4277376523cbc949df631e8
m arun <1998viratarun@gmail.com>
2024-01-18 14:05:22 +0530

    breadcrumb , tabs

--- Breadcrumbs.tsx
+++ Breadcrumbs.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Subtitle from "./Subtitle";
 import { usePathname, useRouter } from "next/navigation";
+import TabList from "./TabList";
 
 interface BreadcrumbsProps {
   children: any;
@@ -13,46 +14,85 @@ const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ children, hideDivider }) => {
   const breads = pathname && pathname?.split("/");
 
   return (
-    <div className={"card w-full mt-1"}>
-      <Subtitle styleClass={"text-[#48a4f9] pl-3"}>
-        <div className="text-sm breadcrumbs">
-          <ul>
-            {breads &&
-              breads?.length > 0 &&
-              breads?.map((ele: any, index: number) => {
-                if (ele && ele !== "") {
-                  return (
-                    <li
-                      key={index}
-                      onClick={() => {
-                        if (index === 1 && breads?.length > 2) {
-                          router.push(`/${ele}`);
-                        } else if (index === 2 && breads?.length > 3) {
-                          router.back();
-                        }
-                      }}
-                      className="capitalize text-black font-[600]  text-[16px]"
-                    >
-                      {index === breads?.length - 1 ? (
-                        ele?.split("-")?.join(" ")
-                      ) : (
-                        <a>{ele?.split("-")?.join(" ")}</a>
-                      )}
-                    </li>
-                  );
-                } else return null;
-              })}
-          </ul>
+    <div className={"card w-full "}>
+      <div className="flex justify-between align-middle h-fit ">
+      <div className=" ml-3">
+          <Tabs />
         </div>
-      </Subtitle>
+        <div className="mr-4 mt-1">
+          <Subtitle styleClass={"text-[#48a4f9] pl-3"}>
+            <div className="text-sm breadcrumbs pt-2 pb-0">
+              <ul>
+                {breads &&
+                  breads?.length > 0 &&
+                  breads?.map((ele: any, index: number) => {
+                    if (ele && ele !== "") {
+                      return (
+                        <li
+                          key={index}
+                          onClick={() => {
+                            if (index === 1 && breads?.length > 2) {
+                              router.push(`/${ele}`);
+                            } else if (index === 2 && breads?.length > 3) {
+                              router.back();
+                            }
+                          }}
+                          className=" ml-1 capitalize text-slate-700 font-[300]  text-xs "
+                        >
+                          {index === breads?.length - 1 ? (
+                            ele?.split("-")?.join(" ")
+                          ) : (
+                            <a>{ele?.split("-")?.join(" ")}</a>
+                          )}
+                        </li>
+                      );
+                    } else return null;
+                  })}
+              </ul>
+            </div>
+          </Subtitle>
+        </div>
+       
+      </div>
       {!hideDivider ? (
-        <div className="divider my-0"></div>
+        <>
+          <div className="my-2 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50"></div>
+        </>
       ) : (
         <div className="mt-5"></div>
       )}
+      <div>
+      </div>
       <div className="h-full w-full pb-6 ">{children}</div>
     </div>
   );
 };
+
+
+
+const Tabs: React.FC = () => {
+  const [activeTab, setActiveTab] = useState('Tab1'); // Set the initial active tab
+
+  // Define your tabs
+  const tabs = ['user', 'Tab2', 'Tab3'];
+
+  // Handle tab change
+  const handleTabChange = (tab: string) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div>
+      {/* Render other content as needed */}
+      <TabList tabs={tabs} activeTab={activeTab} onTabChange={handleTabChange} />
+
+      {/* Render content for the active tab */}
+      {/* {activeTab === 'Tab1' && <div>Content for Tab 1</div>}
+      {activeTab === 'Tab2' && <div>Content for Tab 2</div>}
+      {activeTab === 'Tab3' && <div>Content for Tab 3</div>} */}
+    </div>
+  );
+};
+
 
 export default Breadcrumbs;
--- TabList/index.tsx
+++ TabList/index.tsx
@@ -1,3 +1,6 @@
+import CloseIcon from '@/assets/icons/CloseIcon';
+import DeleteIcon from '@/assets/icons/Delete';
+import NotificationIcon from '@/assets/icons/NotificationIcon';
 import React from 'react';
 
 // Define the props for the TabList component
@@ -10,18 +13,30 @@ interface TabListProps {
 // TabList component
 const TabList: React.FC<TabListProps> = ({ tabs, activeTab, onTabChange }) => {
   return (
-    <div role="tablist" className="tabs tabs-bordered">
-      {tabs.map((tab, index) => (
-        <a
+      <div role="tablist" className="mt-2 bg-[#efefef] tabs tabs-boxed col-span-2">
+        {tabs.map((tab, index) => (
+          <a
           key={index}
           role="tab"
-          className={`tab ${tab === activeTab ? 'tab-active' : ''}`}
+          className={`h-fit py-0.5 text-xs font-light px-2 tab ${tab === activeTab ? 'tab-active' : ''}`}
           onClick={() => onTabChange(tab)}
+          style={{
+            // Set styles dynamically based on the active state
+            backgroundColor: tab === activeTab ? '#1f6698' : '',
+            color: tab === activeTab ? 'your-active-text-color' : 'your-inactive-text-color',
+          }}
         >
-          {tab}
+            <div className='flex'>
+              <span  style={{
+            // Set styles dynamically based on the active state
+            color: tab === activeTab ? '#fff' : '',
+            
+          }} className='text-black'>{tab} </span>
+              <span  className='ml-3 mt-1'><CloseIcon stroke={tab === activeTab ? '#fff' : ''} width='9' height='9' /></span>
+          </div>
         </a>
-      ))}
-    </div>
+        ))}
+      </div>
   );
 };
 
