b6214db26510e4a9db4942a8a981ec374ddbcdc6
m arun <1998viratarun@gmail.com>
2024-01-19 21:25:54 +0530

    svg icons added, changed ui part in table still working on that

--- ButtonGroup/Button.tsx
+++ ButtonGroup/Button.tsx
@@ -1,20 +1,22 @@
 "use client";
 
 interface ButtonProps {
-  icon: React.ReactNode;
-  label: string;
-  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  icon?: React.ReactNode;
+  label?: string;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   btnVariant?: string;
   btnSize?: string;
+  htmlFor?: string;
   // path: string;
 }
 
-const Buttons: React.FC<ButtonProps> = ({ btnSize, icon, label, onClick, btnVariant }) => {
+const Buttons: React.FC<ButtonProps> = ({ htmlFor, btnSize, icon, label, onClick, btnVariant }) => {
   return (
     <>
       <button
+
         onClick={onClick}
-        className={`btn   ${btnSize}  ${btnVariant} btn-primary rounded `}
+        className={`btn font-medium text-xs pt-1 pb-1 min-h-5 h-5 leading-none   ${btnSize}  ${btnVariant}  rounded-sm `}
       >
         {icon}
         {label}
@@ -23,4 +25,22 @@ const Buttons: React.FC<ButtonProps> = ({ btnSize, icon, label, onClick, btnVari
   );
 };
 
+
+const ButtonBorder: React.FC<ButtonProps> = ({ htmlFor, btnSize, icon, label, onClick, btnVariant }) => {
+  return (
+    <>
+      <button
+
+        onClick={onClick}
+        className={`flex justify-center align-middle ${btnSize} text-xs font-light px-2 py-0.5 border border-white  ${btnVariant}  rounded-sm `}
+      >
+       <span className="mt-0.5 mr-1"> {icon}</span>
+        {label}
+      </button>
+    </>
+  );
+};
+
+
+export {ButtonBorder}
 export default Buttons;
